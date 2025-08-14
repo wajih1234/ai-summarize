0f232cbb8104fe6139b9bef107d63cbb9e86c710
wajih1234 <74155158+wajih1234@users.noreply.github.com>
2025-08-14 19:11:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // this is the folder that will be created after running `npm run build`
+  },
 })
